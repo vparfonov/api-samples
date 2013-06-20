99da090e894db33d92c97959f866c7d2cd19ed1f
Vitaly Parfonov <vparfonov@codenvy.com>
2013-06-20 12:18:13 +0300

    Init commit

---
+++ nb-api-samples~api-samples/versions/7.1/misc/Ceylon/src/org/ceylon/filetype/package-info.java
@@ -0,0 +1,10 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+@TemplateRegistration(folder = "Other", displayName = "#Ceylontemplate_displayName", content = "Object.ceylon")
+@Messages(value = "Ceylontemplate_displayName=Ceylon Object file")
+package org.ceylon.filetype;
+
+import org.netbeans.api.templates.TemplateRegistration;
+import org.openide.util.NbBundle.Messages;
